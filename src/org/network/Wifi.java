bab755dc8f08ef03717a1748b5d9c80ba28984de
Litwin <rshibu1011@gmail.com>
2023-09-02 15:47:22 +0530

    Hello

---
+++ src/org/network/Wifi.java
@@ -0,0 +1,25 @@
+package org.network;
+
+public class Wifi {
+	public void wifiName() {
+		
+		System.out.println("Wifi name- Nameless");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Wifi w = new Wifi();
+		w.wifiName();
+		
+		MobileData m = new MobileData();
+		m.dataName();
+		
+		Lan l = new Lan();
+		l.lanName();
+		
+		Wireless wi = new Wireless();
+		wi.modemName();
+	}
+
+}
